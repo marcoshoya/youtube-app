0ffbe18373b7e91361cbf4e7f3d8a66367f9a255
marcoshoya <marcoshoya@gmail.com>
2017-02-24 15:11:13 -0600

    standalone actions

--- app/src/main/java/com/marcoshoya/youtubeapp/MainActivity.java
+++ app/src/main/java/com/marcoshoya/youtubeapp/MainActivity.java
@@ -1,13 +1,45 @@
 package com.marcoshoya.youtubeapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeStandalonePlayer;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSingle = (Button) findViewById(R.id.btnPlaSingle);
+        Button btnStandAlone = (Button) findViewById(R.id.btnStandAlone);
+
+        btnSingle.setOnClickListener(this);
+        btnStandAlone.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = null;
+
+        switch (view.getId()) {
+            case R.id.btnPlaSingle:
+                intent = new Intent(this, YoutubeActivity.class);
+                break;
+
+            case R.id.btnStandAlone:
+                intent = new Intent(this, StandaloneActivity.class);
+                break;
+
+            default:
+        }
+
+        if (intent != null) {
+            startActivity(intent);
+        }
     }
 }
